c5827798eb5d694da2a67b57200bb8d0558317ee
3eDevAndrew <andrew@threeedev.com>
2016-12-02 20:56:24 -0600

    Quiz First Commit

---
+++ EEEAppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  EEEAppDelegate.h
+//  Quiz
+//
+//  Created by Andrew Vasquez on 11/28/16.
+//  Copyright © 2016 Training. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface EEEAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Quiz
+//
+//  Created by Andrew Vasquez on 11/28/16.
+//  Copyright © 2016 Training. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "EEEAppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([EEEAppDelegate class]));
+    }
+}
